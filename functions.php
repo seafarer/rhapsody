8dc8fa2b6745b7b49c9e233fb292ee377322ba4c
seafarer <colin@studiotaraz.com>
2016-11-11 15:32:47 -0700

    Only enqueue main file, rest handled by gulp

--- functions.php
+++ functions.php
@@ -91,13 +91,8 @@ function rhapsody_scripts() {
     wp_enqueue_script('jquery');
   }
 
-  wp_enqueue_script( 'rhapsody-modernizr', get_template_directory_uri() . '/js/vendor/modernizr.custom.js',array(), '2.7.2', false);
-  wp_enqueue_script( 'rhapsody-classie', get_template_directory_uri() . '/js/vendor/classie.js', array(), '1.0.1', true );
-	wp_enqueue_script( 'rhapsody-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '0.0.1', true );
-  wp_enqueue_script( 'rhapsody-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
-  wp_enqueue_script( 'rhapsody-bxslider', get_template_directory_uri() . '/js/vendor/jquery.bxslider.min.js', array(), '4.2.5', true );
-  wp_enqueue_script( 'rhapsody-app', get_template_directory_uri() . '/js/app.js', array(), '1.0.0', true );
-
+  wp_enqueue_script( 'rhapsody-modernizr', get_template_directory_uri() . '/assets/js/modernizr.custom.js',array(), '3.3.1', false);
+  wp_enqueue_script( 'rhapsody-app', get_template_directory_uri() . '/assets/js/all.js', array(), '1.0.0', true );
 
   if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -109,6 +104,11 @@ add_action( 'wp_enqueue_scripts', 'rhapsody_scripts' );
  * Custom template tags for this theme.
  */
 require get_template_directory() . '/inc/template-tags.php';
+
+/**
+ * Custom template tags for this theme.
+ */
+require get_template_directory() . '/inc/extras.php';
 
 /**
  * Load Jetpack compatibility file.
